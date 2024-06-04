a02c0fd7ed4caf14e7721236eeb0b917cb89129e
bouchaiblemaire <128900616+bouchaiblemaire@users.noreply.github.com>
2024-06-04 09:34:15 +0200

    Update run_script_bd.sh

--- run_script_bd.sh
+++ run_script_bd.sh
@@ -10,7 +10,7 @@ fi
 docker exec $1  bash -c "curl -O -L http://einfo-learning.fr/partages/dev_avance/employee_bd.sql.tar.gz &&\
 tar xvfz employee_bd.sql.tar.gz && PGPASSWORD=password psql \
   -h 127.0.0.1 \
-  -p 5432 \
+  -p 5433 \
   -d devavance_db \
   -U devavance_user \
   -f employee_bd.sql&&rm employee_bd.sql.*"

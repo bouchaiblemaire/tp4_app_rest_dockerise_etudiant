db7e42d680ba285841ac9cee2b077e33d2823de1
BL <bouchkhaf@gmail.com>
2024-06-11 12:11:47 +0200

    Correction bug
    Changement nom archive SQL pour permettre le telechargement

--- README.md
+++
@@ -1,2 +0,0 @@
-# tp4_app_rest_dockerise_etudiant
-TP4 dev avancé : Découverte d'une API REST
--- run_script_bd.sh
+++ run_script_bd.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+ARCHIVE_SQL='employee_bd.tar.gz'
+
+
 if [ ! $# -eq 1 ] 
 then
   echo usage : run_script_bd.sh nom_du_conteneur_postgresql
@@ -7,8 +10,8 @@ then
   exit 1
 fi
 
-docker exec $1  bash -c "curl -O -L http://einfo-learning.fr/partages/dev_avance/employee_bd.sql.tar.gz &&\
-tar xvfz employee_bd.sql.tar.gz && PGPASSWORD=password psql \
+docker exec $1  bash -c "curl -O -L http://einfo-learning.fr/partages/dev_avance/${ARCHIVE_SQL} &&\
+tar xvfz ${ARCHIVE_SQL} && PGPASSWORD=password psql \
   -h 127.0.0.1 \
   -p 5432 \
   -d devavance_db \
